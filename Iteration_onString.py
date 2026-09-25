dd3bb16f1e4fda357d101f3eef4ec8d220e4bb83
Sanjana Umate <sanjanaumate19@gmail.com>
2026-09-25 15:30:00 +0530

    Add files via upload

---
+++ Iteration_onString.py
@@ -0,0 +1,16 @@
+name = "sanjana"
+for char in name:
+    print(char)
+
+n1 = "Laptop"
+for char in n1:
+    print(char)
+
+n2 = "Operators"
+for i in n2:
+    print(n2)
+
+name1 = "pune"
+for char in name1:
+    print(name1)
+    print(char)
